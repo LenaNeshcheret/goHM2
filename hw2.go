3bf1888c330d1db1aed25cb1f448525283ba9253
bigmag <awaywith2@icloud.com>
2024-05-22 09:18:31 +0200

    hW 2 - second commit

--- hw2.go
+++ hw2.go
@@ -2,73 +2,20 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 )
 
 func main() {
-	// Initialize the animals
-	animals := []Animal{
-		{"Elephant", "Dambo", 3, 2},
-		{"Lion", "Leo", 2, 1.51},
-		{"Monkey", "McScruff", 0.53, 0.52},
-		{"Giraffe", "Dottie", 5.01, 1.8},
-		{"Tiger", "Teo", 2.57, 1.23},
-		{"Panda", "Bo-bo", 1.85, 1.23},
-		{"Zebra", "Ziggy", 2.3, 1.4},
-		{"Kangaroo", "Kenny", 1.9, 0.8},
-		{"Crocodile", "Snappy", 4.5, 80},
-		{"Penguin", "Waddles", 0.7, 5},
-		{"Hippo", "Bubbles", 4.2, 2.5},
-		{"Gorilla", "Gigi", 1.95, 160},
-		{"Parrot", "Polly", 0.3, 0.4},
-		{"Ostrich", "Ozzy", 2.1, 10},
-		{"Rhino", "Rex", 1.8, 2.3},
-		{"Turtle", "Sheldon", 0.2, 0.25},
-		{"Polar Bear", "Snowy", 1.6, 350},
-		{"Koala", "Kobi", 0.6, 12},
-		{"Cheetah", "Chet", 1.1, 60},
-		{"Sloth", "Sid", 0.5, 8},
-		{"Giraffe", "Stretch", 5.3, 2},
-		{"Penguin", "Pingu", 0.65, 4},
-		{"Panda", "Pandy", 1.75, 1.1},
-		{"Elephant", "Jumbo", 3.5, 3.2},
-	}
+	animals := initializeAllAnimalsInZoo()
 
-	// Initialize the zookeeper
 	zookeeper := Zookeeper{Name: "John"}
 
-	// Initialize the cages
-	cage1 := Cage{
-		Name:        "Cage 1",
-		AnimalsInfo: []string{"Dambo", "Dottie", "Ziggy", "Kenny", "Rex", "Ozzy", "Stretch"},
-		Animals: []Animal{
-			{"Elephant", "Dambo", 3, 2},
-			{"Giraffe", "Dottie", 5.01, 1.8},
-			{"Zebra", "Ziggy", 2.3, 1.4},
-		},
-	}
-	cage2 := Cage{
-		Name:        "Cage 2",
-		AnimalsInfo: []string{"Ziggy", "Waddles", "Pingu"},
-		Animals: []Animal{
-			{"Zebra", "Ziggy", 2.3, 1.4},
-			{"Penguin", "Waddles", 0.7, 5},
-			{"Penguin", "Pingu", 0.65, 4},
-		},
-	}
-	cage3 := Cage{
-		Name:        "Cage 3",
-		AnimalsInfo: []string{"Bo-bo", "Pandy", "Jumbo"},
-		Animals: []Animal{
-			{"Panda", "Bo-bo", 1.85, 1.23},
-			{"Panda", "Pandy", 1.75, 1.1},
-		},
-	}
-	cages := []Cage{cage1, cage2, cage3}
+	cages := InitializeCages(animals)
 
-	// Initialize the zoo
 	zoo := Zoo{Zookeeper: zookeeper, Cages: cages, Animals: animals}
 
-	// Check for escaped animals
+	randomEscape(&zoo)
+
 	checkEscapedAnimals(zoo)
 }
 
@@ -93,6 +40,21 @@ type Zoo struct {
 	Zookeeper Zookeeper
 	Cages     []Cage
 	Animals   []Animal
+}
+
+// Function to deleted animals (escaped animals)
+func randomEscape(zoo *Zoo) {
+	for i := range zoo.Cages {
+		var remainingAnimals []Animal
+		for _, animal := range zoo.Cages[i].Animals {
+			randomValue := rand.Intn(2)
+			if randomValue == 1 {
+			} else {
+				remainingAnimals = append(remainingAnimals, animal)
+			}
+		}
+		zoo.Cages[i].Animals = remainingAnimals
+	}
 }
 
 // Function to check for escaped animals and print their details
@@ -130,4 +92,56 @@ func getAnimalByName(name string, animals []Animal) Animal {
 		}
 	}
 	return Animal{}
+}
+
+func initializeAllAnimalsInZoo() []Animal {
+	return []Animal{
+		{"Elephant", "Dambo", 3, 2}, //0
+		{"Elephant", "Jumbo", 3.5, 3.2},
+		{"Giraffe", "Dottie", 5.01, 1.8},
+		{"Giraffe", "Stretch", 5.3, 2},
+		{"Zebra", "Ziggy", 2.3, 1.4},
+		{"Kangaroo", "Kenny", 1.9, 0.8},
+		{"Rhino", "Rex", 1.8, 2.3},
+		{"Ostrich", "Ozzy", 2.1, 10},
+		{"Panda", "Bo-bo", 1.85, 1.23},
+		{"Panda", "Pandy", 1.75, 1.1},
+		{"Koala", "Kobi", 0.6, 12}, //10
+		{"Sloth", "Sid", 0.5, 8},   //11
+		{"Penguin", "Waddles", 0.7, 5},
+		{"Penguin", "Pingu", 0.65, 4},
+		{"Hippo", "Bubbles", 4.2, 2.5},
+		{"Gorilla", "Gigi", 1.95, 160},
+		{"Parrot", "Polly", 0.3, 0.4},
+		{"Turtle", "Sheldon", 0.2, 0.25},
+		{"Polar Bear", "Snowy", 1.6, 350}, //18
+		{"Lion", "Leo", 2, 1.51},          //19
+		{"Tiger", "Teo", 2.57, 1.23},
+		{"Crocodile", "Snappy", 4.5, 80},
+		{"Cheetah", "Chet", 1.1, 60}, //22
+	}
+}
+
+func InitializeCages(animals []Animal) []Cage {
+	animalGroup1 := animals[:10]
+	cage1 := Cage{
+		Name: "Cage 1",
+		AnimalsInfo: []string{"Dambo", "Jumbo", "Dottie", "Stretch", "Ziggy", "Kenny", "Rex", "Ozzy", "Bo-bo",
+			"Pandy", "Kobi"},
+		Animals: animalGroup1,
+	}
+	animalGroup2 := animals[11:18]
+	cage2 := Cage{
+		Name:        "Cage 2",
+		AnimalsInfo: []string{"Waddles", "Pingu", "Bubbles", "Gigi", "Polly", "Sheldon", "Snowy"},
+		Animals:     animalGroup2,
+	}
+	animalGroup3 := animals[19:]
+	cage3 := Cage{
+		Name: "Cage 3",
+		// TODO collect names from animals arr
+		AnimalsInfo: []string{"Leo", "Teo", "Snappy", "Chet"},
+		Animals:     animalGroup3,
+	}
+	return []Cage{cage1, cage2, cage3}
 }
